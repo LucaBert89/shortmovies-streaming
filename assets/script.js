f0de11ffc5153def4090347383944992eefe8362
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-07-19 23:34:32 +0200

    modified

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,46 @@
-var current = 0;
-var filmGallery = document.getElementById("film-gallery");
-var filmGallery2 = document.getElementById("film-gallery-2");
-var slides = document.getElementsByClassName("slide");
+let current = 0;
+const filmGallery = document.getElementById("film-gallery");
+const filmGallery2 = document.getElementById("film-gallery-2");
+const slides = document.getElementsByClassName("slide");
 
-var Hamburger = document.getElementById("menu-hamb");
+const Hamburger = document.getElementById("menu-hamb");
+const underMenu = document.getElementsByClassName("under-menu");
+const mobileMenu = document.getElementById("mobile-menu");
 
-var galleryInner = document.getElementById("inner-slide");
-var galleryInner2 = document.getElementById("inner-slide-2");
+const galleryInner = document.getElementById("inner-slide");
+const galleryInner2 = document.getElementById("inner-slide-2");
 var maxSlide = slides.length;
+const GalleryNavBtns = document.getElementsByClassName("gallery-nav-btn");
 
-var parent1;
 
-var bp = {
+const bp = {
     'xs' : 2,
     'md': 4,
     'lg' : 6,
     'xl' : 8
 };
 
-var pages;
-var GalleryNavBtns = document.getElementsByClassName("gallery-nav-btn");
-
-var vw;
-
-var resizeTimeout;
+let pages;
 
 
-Hamburger.addEventListener("click", openMenu);
+let vw;
 
-function openMenu () {
-    var mobileMenu = document.getElementById("mobile-menu");
-    var underMenu = document.getElementsByClassName("under-menu");
-    var backgroundMobile = document.getElementById("background-mobile");
-    console.log(backgroundMobile);
-    if (mobileMenu.className !== "background-mobile") {
-        mobileMenu.setAttribute("class", "background-mobile");
-        underMenu[0].classList.add("mobile-list-menu");
-        console.log(mobileMenu);
-    } else {
-        mobileMenu.classList.remove("background-mobile");
-        underMenu[0].classList.remove("mobile-list-menu");
-    }
-}
+let resizeTimeout;
+
 
 /*modal */
-var loginBtn = document.getElementById("login-btn");
-var SignBtn = document.getElementById("signup-btn");
-var closeBtn = document.getElementsByClassName("close-btn");
-var ModalLogin = document.getElementById("modal-overlay-log");
-var ModalSign = document.getElementById("modal-overlay-signup");
-var ModalForm = document.getElementById("modal");
-var GoOtherModal = document.getElementsByClassName("go-to-modal");
-
-loginBtn.addEventListener("click", OpenModal);
-SignBtn.addEventListener("click", OpenModalSign);
-for(i=0; i < GoOtherModal.length; i++) {
-GoOtherModal[i].addEventListener("click", ChangeModal);
-}
-
-for(i=0; i < closeBtn.length; i++) {
-closeBtn[i].addEventListener("click", CloseModal);
-}
-
-ModalLogin.addEventListener("click", CloseModal);
-ModalSign.addEventListener("click", CloseModal);
+const loginBtn = document.getElementById("login-btn");
+const SignBtn = document.getElementById("signup-btn");
+const closeBtn = document.getElementsByClassName("close-btn");
+const ModalLogin = document.getElementById("modal-overlay-log");
+const ModalSign = document.getElementById("modal-overlay-signup");
+const ModalForm = document.getElementById("modal");
+const GoOtherModal = document.getElementsByClassName("go-to-modal");
 
 /*END modal */
 
-function init(){
+
+(function init(){
     setupSlideshow();
 
 
@@ -79,32 +52,40 @@ function init(){
         clearTimeout(resizeTimeout);
         resizeTimeout= setTimeout (handleResize, 100);
     } 
-}
 
+    Hamburger.addEventListener("click", openMenu);
 
-init();
+    for(i=0; i < underMenu.length; i++) {
+        underMenu[i].addEventListener("click", closeBackground);
+    }
+    
+    loginBtn.addEventListener("click", OpenModal);
+    SignBtn.addEventListener("click", OpenModalSign);
+    for(i=0; i < GoOtherModal.length; i++) {
+    GoOtherModal[i].addEventListener("click", ChangeModal);
+    }
+    
+    for(i=0; i < closeBtn.length; i++) {
+    closeBtn[i].addEventListener("click", CloseModal);
+    }
+    
+    ModalLogin.addEventListener("click", CloseModal);
+    ModalSign.addEventListener("click", CloseModal);
 
-function handleResize () {
-    setupSlideshow();
-}
+})();
 
 function setupSlideshow() {
     vw = window.innerWidth;
     var breakpoint;
-    var pages;
 
     if(vw > 0 && vw <= 768) {
         breakpoint = "xs";
-        pages = [1,2,3,4,5,6,7,8,9,10,11,12];
     } else if (vw > 768 && vw <= 992) {
         breakpoint = "md";
-        pages = [1,2,3,4,5,6];
     } else if (vw > 992 && vw <= 1200) {
         breakpoint = "lg";
-        pages = [1,2,3,4]
     } else {
         breakpoint = "xl";
-        pages = [1,2,3]
     }
 
     var newview = vw / bp[breakpoint];
@@ -116,10 +97,13 @@ function setupSlideshow() {
  
 }
 
+function handleResize () {
+    setupSlideshow();
+}
 
-function handleBtnClick (event) {
+
+function handleBtnClick () {
     var BtnClicked = this;
-    var pages;
 
     if(vw > 0 && vw <= 768) {
         pages = [1,2,3,4,5,6,7,8,9,10,11,12];
@@ -136,13 +120,10 @@ function handleBtnClick (event) {
     var next = 0;
     if(parent === "film-gallery") {
         if(action === "p") {
-            console.log(parent1);
             next = current > 0 ? current - 1 : pages.length-1 ;
-            console.log(next);
         } else if(action === "n") {
             
             next = current < pages.length-1 ? current + 1 : 0;        
-            console.log(current);
         }} else if (parent === "film-gallery-2") {
             if(action === "p2") {
                 next = current > 0 ? current - 1 : pages.length-1;
@@ -154,11 +135,10 @@ function handleBtnClick (event) {
     } 
     
     function gotoSlide(next) {
-        
         if(current == next) {
             return;
         }
-        var newOffset = vw * next; 
+        let newOffset = vw * next; 
         if(parent === "film-gallery") {
         galleryInner.style.transform = "translateX(" + ( - newOffset) + "px)";
         } else {
@@ -168,7 +148,7 @@ function handleBtnClick (event) {
     }
 
 
-function OpenModal (event) {
+function OpenModal () {
     ModalLogin.style.display = "flex";
 }
 
@@ -176,7 +156,7 @@ function OpenModalSign() {
     ModalSign.style.display = "flex";
 }
 
-function ChangeModal (event) {
+function ChangeModal () {
 
     if (this.innerText == "Login Now!") {
         ModalSign.style.display = "none";
@@ -188,10 +168,27 @@ function ChangeModal (event) {
 }
 
 function CloseModal(event) {
-    console.log(event.target);
     if(event.target.getAttribute("id") !== "modal-overlay-log" && event.target.getAttribute("id") !== "modal-overlay-signup" && event.target.getAttribute("class") !== "far fa-times-circle") {
         return;
     }
     ModalLogin.style.display = "none";
     ModalSign.style.display = "none";
+}
+
+function closeBackground() {
+    mobileMenu.classList.remove("background-mobile");
+    underMenu[0].classList.remove("mobile-list-menu");
+}
+
+function openMenu () {
+    var backgroundMobile = document.getElementById("background-mobile");
+    console.log(backgroundMobile);
+    if (mobileMenu.className !== "background-mobile") {
+        mobileMenu.setAttribute("class", "background-mobile");
+        underMenu[0].classList.add("mobile-list-menu");
+        console.log(mobileMenu);
+    } else {
+        mobileMenu.classList.remove("background-mobile");
+        underMenu[0].classList.remove("mobile-list-menu");
+    }
 }
